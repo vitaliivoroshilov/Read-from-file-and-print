4b4761e4183d1dd13e36d22f587ade06d47f3b27
vitaliivoroshilov <73603291+vitaliivoroshilov@users.noreply.github.com>
2022-11-10 22:58:04 +0300

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -6,20 +6,20 @@ int main()
 {
     char name[250];
     string s;
-    cout << "Enter the name of the file: "; //Ввести путь к файлу
+    cout << "Enter the name of the file: "; //File path
     cin.getline(name, 250);
     FILE* f;
     fopen_s(&f, name, "r");
 
-    fseek(f, 0, SEEK_END); //Сместим указатель f к концу файла
-    int size = ftell(f); //Получаем размер файла
+    fseek(f, 0, SEEK_END); //Move the pointer to the end of file
+    int size = ftell(f); //Get size of file
     fseek(f, 0, SEEK_SET);
 
     char buf(size);
 
     while (!feof(f))
     {
-        fread(&buf, sizeof buf, 1, f); //считываем посимвольно из файла
+        fread(&buf, sizeof buf, 1, f); //Read from a file
         if (isalpha(buf)) cout << " " << buf;
         else cout << " \n Unknown parameter " << buf << "\n";
     }
